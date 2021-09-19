5a5df728fba22ace9c052855150ff22f3ed2d47b
Daniel Gil <dgilalegre96@gmail.com>
2021-09-19 18:21:39 +0200

    Lesson 4

---
+++ 04-webpack/src/script.js
@@ -0,0 +1,36 @@
+import "./style.css";
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+
+// Scene
+const scene = new Scene();
+
+// Object
+const geometry = new BoxGeometry(1, 1, 1);
+const material = new MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new WebGLRenderer({
+  canvas: document.querySelector("canvas.webgl"),
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);

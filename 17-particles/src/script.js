4a13fc4adaf83b47a5b79b2d86dbbe78e5fe5bde
Daniel Gil Alegre <dgilalegre96@gmail.com>
2021-09-23 20:41:14 +0200

    Lesson 17 finished

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -2,6 +2,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
+import { BufferAttribute } from "three";
 
 /**
  * Base
@@ -19,21 +20,59 @@ const scene = new THREE.Scene();
  * Textures
  */
 const textureLoader = new THREE.TextureLoader();
+const particleTexture = textureLoader.load("/textures/particles/2.png");
 
 /**
  * Particles
  */
+// // Geometry
+// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+
 // Geometry
-const particlesGeometry = new THREE.SphereGeometry(1, 32, 32);
+const particlesGeometry = new THREE.BufferGeometry();
+const count = 20000;
+
+const position = new Float32Array(count * 3); // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const colors = new Float32Array(count * 3);
+
+for (
+  let i = 0;
+  i < count * 3;
+  i++ // Multiply by 3 for same reason
+) {
+  position[i] = (Math.random() - 0.5) * 10; // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+  colors[i] = Math.random();
+}
+
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(position, 3)
+); // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+particlesGeometry.setAttribute("color", new THREE.BufferAttribute(colors, 3));
 
 // Material
 const particlesMaterial = new THREE.PointsMaterial();
-particlesMaterial.size = 0.02;
+particlesMaterial.size = 0.1;
 particlesMaterial.sizeAttenuation = true;
+// particlesMaterial.color = new THREE.Color("#ff88cc");
+particlesMaterial.transparent = true;
+particlesMaterial.alphaMap = particleTexture;
+// particlesMaterial.alphaTest = 0.001;
+// particlesMaterial.depthTest = false;
+particlesMaterial.depthWrite = false;
+particlesMaterial.blending = THREE.AdditiveBlending;
+particlesMaterial.vertexColors = true;
 
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial);
 scene.add(particles);
+
+// // Cube
+// const cube = new THREE.Mesh(
+//   new THREE.BoxBufferGeometry(),
+//   new THREE.MeshBasicMaterial()
+// );
+// scene.add(cube);
 
 /**
  * Sizes
@@ -90,6 +129,18 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Update particles
+  // particles.rotation.y = elapsedTime * 0.2;
+  for (let i = 0; i < count; i++) {
+    let i3 = i * 3;
+
+    const x = particlesGeometry.attributes.position.array[i3];
+    particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(
+      elapsedTime + x
+    );
+  }
+  particlesGeometry.attributes.position.needsUpdate = true;
 
   // Update controls
   controls.update();

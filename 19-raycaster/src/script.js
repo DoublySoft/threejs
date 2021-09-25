69a09607060b5acee7161ab99833f14573a96f47
Daniel Gil Alegre <dgilalegre96@gmail.com>
2021-09-25 14:00:48 +0200

    Lesson 19 finished

--- 19-raycaster/src/script.js
+++ 19-raycaster/src/script.js
@@ -82,88 +82,106 @@ window.addEventListener('resize', () =>
  */
 const mouse = new THREE.Vector2();
 
-window.addEventListener('mousemove', (event) => {
-    mouse.x = event.clientX / sizes.width * 2 - 1;
-    mouse.y = event.clientY / sizes.height * 2 - 1;
-})
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / sizes.width) * 2 - 1;
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1;
+});
+
+window.addEventListener("click", () => {
+  if (currentIntersect) {
+    switch (currentIntersect.object) {
+      case object1:
+        console.log("Clock on object 1");
+        break;
+      case object2:
+        console.log("Clock on object 2");
+        break;
+      case object3:
+        console.log("Clock on object 3");
+        break;
+    }
+  }
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 3;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
 let currentIntersect = null;
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Animate objects
-    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
-    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
-    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
+  // Animate objects
+  object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5;
 
-    // // Cast a ray
-    // const rayOrigin = new THREE.Vector3(-3, 0, 0);
-    // const rayDirection = new THREE.Vector3(1, 0, 0);
-    // rayDirection.normalize();
+  // // Cast a ray
+  raycaster.setFromCamera(mouse, camera);
 
-    // raycaster.set(rayOrigin, rayDirection);
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
 
-    // const objectsToTest = [object1, object2, object3];
-    // const intersects = raycaster.intersectObjects(objectsToTest);
+  for (const object of objectsToTest) {
+    object.material.color.set("#ff0000");
+  }
 
-    // for (const object of objectsToTest) {
-    //     object.material.color.set('#ff0000');
+  for (const intersect of intersects) {
+    // if (!intersects.find(intersect => intersect.object === object)) {
+    intersect.object.material.color.set("#0000ff");
     // }
-    // for (const intersect of intersects) {
-    //     intersect.object.material.color.set("#0000ff");
-    // }
+  }
 
-    raycaster.setFromCamera(mouse, camera);
-
-    const objectsToTest = [object1, object2, object3];
-    const intersects = raycaster.intersectObjects(objectsToTest);
-
-    for(const object of objectsToTest) {
-        object.material.color.set("#ff0000");
-    }
-    
-    for (const intersect of intersects) {
-        // if (!intersects.find(intersect => intersect.object === object)) {
-            intersect.object.material.color.set("#0000ff");
-        // }
+  if (intersects.length) {
+    if (!currentIntersect) {
+      console.log("Mouse enter");
     }
 
-    // Update controls
-    controls.update()
+    currentIntersect = intersects[0];
+  } else {
+    if (currentIntersect) {
+      console.log("Mouse leave");
+    }
 
-    // Render
-    renderer.render(scene, camera)
+    currentIntersect = null;
+  }
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
 
 tick()

ed612934b5730432976050fe21e418cef969bdf3
Daniel Gil Alegre <dgilalegre96@gmail.com>
2022-11-04 20:43:02 +0100

    Added lesson 24 skeleton

--- 24-custom-models-with-blender/src/script.js
+++ 24-custom-models-with-blender/src/script.js
@@ -3,7 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-import * as dat from 'dat.gui'
+import * as dat from 'lil-gui'
 
 /**
  * Base
